f1c4a3d848b51de7ae112456043268345f7cc0e4
Oleksii <tornado@gmail.com>
2022-10-23 20:42:54 +0300

    finish main blocks

--- src/js/main.js
+++ src/js/main.js
@@ -9,3 +9,27 @@ new Swiper(".swiper", {
        
           
   });
+
+// модальне вікно
+
+const button = document.getElementById('openContact'),
+      navbarBtn = document.querySelector('.navbar__btn'),
+      overflow = document.querySelector('.overflow'),
+      modal = document.getElementById('consultation'),
+      closeBtn = document.querySelector('.modal-consult__close');
+
+button.addEventListener('click', () => {
+  modal.classList.add('active');
+  overflow.classList.add('active');
+  
+});
+navbarBtn.addEventListener('click', () => {
+  modal.classList.toggle('active');
+  overflow.classList.toggle('active');
+  
+});
+closeBtn.addEventListener('click', () => {
+  modal.classList.remove('active');
+  overflow.classList.remove('active');
+  
+});
---
+++ src/links/portfolio/src/js/scriptJq.js
@@ -0,0 +1,22 @@
+$().ready(function() {
+
+    $('#send').submit(function(e) {
+        e.preventDefault();
+        if (!$(this).valid()) {
+            return;
+        }
+        $.ajax({
+            type: "POST",
+            url: "/mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function(){
+            $(this).find("input").val("");
+            $(this).find("textarea").val("");
+            $('#send').trigger('reset');
+        });
+        return false;
+        });
+});
+
+
+ 
